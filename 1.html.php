2a49fc7e4e26433be34246e3b49cd307f1261fad
王韋竣 Must <89266444+aparn0168@users.noreply.github.com>
2021-11-05 13:49:32 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,20 @@
+   
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> <?php #程式重點 在html裡面使用PHP?>
+        <?php #PHP寫法 echo(輸出) "字串"?>
+
+        <h1>html</h1>
+        <?php #html寫法 <h1> 內容 </h1>?>
+
+        <?php echo "Hello world!"; ?> 
+        <?php #PHP寫法 echo(輸出) "字串"?>
+
+        <h1>html</h1>
+        <?php #html寫法 <h1> 內容 </h1>?>
+
+        <?php echo "Hello world!"; ?>
+        <?php #PHP寫法 echo(輸出) "字串"?>
+    </body>
+</html>
